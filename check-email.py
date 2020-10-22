b7b8a1eb059b7e1f38cbd2844eadda80e821d88d
ergunsahe <66415270+ergunsahe@users.noreply.github.com>
2020-10-23 01:13:42 +0200

    fix:add some new check points

---
+++ check-email.py
@@ -0,0 +1,34 @@
+email = input("please write your email")
+invalid_letters=["!","#","$","%","&","/","+", "<",">","%", ")", "(", "€", "{", "}", "*", "^", "~","£", "="]
+
+for i in invalid_letters:
+    if email.count(i)> 0:
+        message= "invalid email 1"
+        break
+        
+    else:
+        if email.count("@")<1:
+            message= "invalid email 2"
+            
+        elif email.count("@")>1:
+            message= "invalid email 3"
+            
+        else:  
+            split_email=email.split("@")
+            if split_email[1].count(".")<1:
+                message= "invalid email 4"
+                
+                
+            elif split_email[1].count(".")>2:
+                message= "invalid email 5"
+                
+            else:
+                extension=split_email[1].split(".")
+                if len(extension[1])<2 or len(extension[1])>3:
+                    message= "invalid email 6"
+                    
+                    
+                else:
+                    message="valid email"
+                    
+print(message)
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const email = document.querySelector("#email");
 const btn = document.querySelector("#btn");
 const result = document.querySelector("#result")
 
-const invalid_letters=["!","#","$","%","&","/","+", "<",">","%", ")", "(", "€", "{", "}", "*", "^", "~","£", "=", "?", ","]
+const invalid_letters=["!","#","$","%","&","/","+", "<",">","%", ")", "(", "€", "{", "}", "*", "^", "~","£", "=", "?", ",", " ", "ö", "ü", "ğ", "ş", "\'"]
+const letters = /^[0-9a-zA-Z,'@','.']+$/;
 const check_email= () => {
     const split_email=[]
     for (i=0;i<email.value.length;i++){
@@ -13,29 +14,51 @@ const check_email= () => {
      })
     if (n.length>0){
         result.innerText="invalid entry 1"
-        console.log("invalid entry 1")
-    }else if(!(split_email.includes("@"))){
+        email.focus()
+        email.value=""
+        
+    }else if (!isNaN(split_email[0])){
         result.innerText="invalid entry 1-1"
-        console.log("invalid entry 1-1")
+        email.focus()
+        email.value=""
+    }else if(!(split_email.includes("@"))){
+        result.innerText="invalid entry 1-2"
+        email.focus()
+        email.value=""
+        
     }else if (email.value.split("@").length> 2 ){
         result.innerText="invalid entry 2"
-        console.log("invalid entry 2")
+        email.focus()
+        email.value=""
+        
     }else if (!(email.value.split("@")[1].includes("."))){
         result.innerText="invalid entry 3"
-        console.log("invalid entry 3")
+        email.focus()
+        email.value=""
+        
     } else if (email.value.split("@")[1].split(".").length>3){
         result.innerText="invalid entry 4"
-        console.log("invalid entry 4")
+        email.focus()
+        email.value=""
         
+        
+    }else if (!email.value.match(letters)){
+        result.innerText="invalid entry 6"
+        email.focus()
+        email.value=""
     }else if (email.value.split("@")[1].split(".")){
         const m =email.value.split("@")[1].split(".").length
         const k = email.value.split("@")[1].split(".")[m-1];
         if (k.length>3 || k.length<2){
-            result.innerText="invalid entry 5"
-            console.log("invalid entry 5")
+            result.innerText="invalid entry 7"
+            email.focus()
+            email.value=""
+            
         }else if (email) {
             result.innerText="valid entry"
-        console.log("valid entry")
+            email.focus()
+            email.value=""
+            
         }
     }
 
